2fa27b6792206b5cc8b4ef483634164e7408d18a
Vasu Parashar <136962787+whotfvasu@users.noreply.github.com>
2024-06-17 02:55:10 +0000

    data types

---
+++ 01_basics/01_data_types.js
@@ -0,0 +1,9 @@
+// number => 2 to the power 53
+// bigint
+// null => empty value (stand alone value) => object
+// undefined => value is not defined => type
+// symbol => unique 
+// object
+
+console.log(typeof null)
+console.log(typeof undefined);
