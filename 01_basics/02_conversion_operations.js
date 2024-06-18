c409adb7cc818e82aea35434ac42cddc9e927dea
Vasu Parashar <136962787+whotfvasu@users.noreply.github.com>
2024-06-18 14:07:16 +0000

    conversion operations

---
+++ 01_basics/02_conversion_operations.js
@@ -0,0 +1,13 @@
+let score = "33"
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// NaN => 0
+// undefined => NaN
+
